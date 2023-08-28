fb2c5a36668c8b8e2e4835f01dc762b6408b8a50
Klemciak <klemcio@interia.pl>
2023-08-28 19:20:22 +0200

    persons public server

--- MainSite/JS/main.js
+++ MainSite/JS/main.js
@@ -1,3 +1,8 @@
+
+// 
+//   show Popup 
+// 
+
 const popUpWrap = document.querySelector('.popUpWrap');
 const editPopUp = document.querySelector('.editPopUp');
 const afterPopUp = document.querySelector('.afterPopUp');
@@ -14,3 +19,83 @@ const showHidePopup = (e) => {
 editBtn.addEventListener('click', showHidePopup);
 btnX.addEventListener('click', showHidePopup);
 cancelBtn.addEventListener('click', showHidePopup);
+
+//
+//  add persons
+// 
+
+// DOMContentLoaded
+{/* <div class="person">
+                <div class="person__dot">NU</div>
+                <span class="person__name">Marek</span>
+                <span class="person__surname">Rostowski</span>
+                <span class="person__email">m.rostowski@mail.pl</span>
+                <span class="person__date">20.12.1988</span>
+            </div> */}
+
+
+document.addEventListener("DOMContentLoaded", (e) => {
+
+
+    fetch('http://api.ultimate.systems/public/index.php/api/v1/auth/users?filter%5Bis_activated%5D=ACTIVE%2CINACTIVE&sort%5Bid%5D=asc&sort%5Bname%5D=asc&sort%5Bsurname%5D=asc&sort%5Bbirth_date%5D=asc&page=1&perPage=1000', {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+    }).then((res) => {
+        console.log(res)
+        res.json()
+            .then((users) => {
+                console.log(users.data)
+                let array = [...users.data]
+                array.forEach(item => {
+                    const persons = document.querySelector('.persons');
+
+                    let person = document.createElement('div');
+                    person.className = 'person';
+
+
+                    let personDot = document.createElement('div');
+                    personDot.className = 'person__dot';
+                    personDot.innerHTML = item.id
+
+
+                    let personName = document.createElement('span');
+                    personName.className = 'person__name';
+                    personName.innerHTML = item.name
+
+
+                    let personSurname = document.createElement('span');
+                    personSurname.className = "person__surname";
+                    personSurname.innerHTML = item.surname
+
+
+                    let personEmail = document.createElement('span');
+                    personEmail.className = "person__email";
+                    personEmail.innerHTML = item.email
+
+
+                    let personDate = document.createElement('span');
+                    personDate.className = "person__date";
+                    personDate.innerHTML = item.birth_date
+
+
+
+
+
+
+                    persons.appendChild(person);
+
+                    person.appendChild(personDot);
+                    person.appendChild(personName);
+                    person.appendChild(personSurname);
+                    person.appendChild(personEmail);
+                    person.appendChild(personDate);
+                })
+
+
+            })
+    }).catch(error => console.log('ERROR'))
+
+})
+
