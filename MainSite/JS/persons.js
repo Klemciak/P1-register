9896eedb5a8a516d8eff7673fa89aa7237d4904d
Klemciak <klemcio@interia.pl>
2023-08-29 12:20:34 +0200

    Popup errors

--- MainSite/JS/persons.js
+++ MainSite/JS/persons.js
@@ -1,52 +1,12 @@
-
-// 
-//   show Popup 
-// 
-
-const popUpWrap = document.querySelector('.popUpWrap');
-const editPopUp = document.querySelector('.editPopUp');
-const afterPopUp = document.querySelector('.afterPopUp');
-
-const editBtn = document.querySelector('.editAccount__button');
-const cancelBtn = document.querySelector('.editPopUp__buttons__cancel');
-const btnX = document.querySelector('.editPopUp__exit');
-
-const showHidePopup = (e) => {
-    popUpWrap.classList.toggle('active');
-    console.log('działa?');
-}
-
-editBtn.addEventListener('click', showHidePopup);
-btnX.addEventListener('click', showHidePopup);
-cancelBtn.addEventListener('click', showHidePopup);
-
-//
-//  add persons
-// 
-
-// DOMContentLoaded
-{/* <div class="person">
-                <div class="person__dot">NU</div>
-                <span class="person__name">Marek</span>
-                <span class="person__surname">Rostowski</span>
-                <span class="person__email">m.rostowski@mail.pl</span>
-                <span class="person__date">20.12.1988</span>
-            </div> */}
-
-
 document.addEventListener("DOMContentLoaded", (e) => {
-
-
     fetch('http://api.ultimate.systems/public/index.php/api/v1/auth/users?filter%5Bis_activated%5D=ACTIVE%2CINACTIVE&sort%5Bid%5D=asc&sort%5Bname%5D=asc&sort%5Bsurname%5D=asc&sort%5Bbirth_date%5D=asc&page=1&perPage=1000', {
         method: 'GET',
         headers: {
             'Content-Type': 'application/json'
         },
     }).then((res) => {
-        console.log(res)
         res.json()
             .then((users) => {
-                console.log(users.data)
                 let array = [...users.data]
                 array.forEach(item => {
                     const persons = document.querySelector('.persons');
@@ -56,9 +16,13 @@ document.addEventListener("DOMContentLoaded", (e) => {
 
 
                     let personDot = document.createElement('div');
-                    personDot.className = 'person__dot';
-                    personDot.innerHTML = item.id
-
+                    if (item.is_activated === false) {
+                        personDot.className = 'person__dot1';
+                        personDot.innerHTML = 'OFF'
+                    } else {
+                        personDot.className = 'person__dot2';
+                        personDot.innerHTML = 'ON'
+                    }
 
                     let personName = document.createElement('span');
                     personName.className = 'person__name';
@@ -98,4 +62,3 @@ document.addEventListener("DOMContentLoaded", (e) => {
     }).catch(error => console.log('ERROR'))
 
 })
-
---
+++ MainSite/JS/showPopup.js
@@ -0,0 +1,16 @@
+const popUpWrap = document.querySelector('.popUpWrap');
+const editPopUp = document.querySelector('.editPopUp');
+const afterPopUp = document.querySelector('.afterPopUp');
+
+const editBtn = document.querySelector('.editAccount__button');
+const cancelBtn = document.querySelector('.editPopUp__buttons__cancel');
+const btnX = document.querySelector('.editPopUp__exit');
+
+const showHidePopup = (e) => {
+    e.preventDefault();
+    popUpWrap.classList.toggle('active');
+}
+
+editBtn.addEventListener('click', showHidePopup);
+btnX.addEventListener('click', showHidePopup);
+cancelBtn.addEventListener('click', showHidePopup);
